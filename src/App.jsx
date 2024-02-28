75df676c80448d5d9a767bb0b5cbde55a8ec9df3
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-28 15:27:22 +0200

    Render the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Nav } from './cmps/Nav'
+import { Sidebar } from './cmps/Sidebar'
 import { SubMenu } from './cmps/SubMenu'
 
 const App = () => {
   return <main>
     <Nav />
     <SubMenu />
+    <Sidebar />
   </main>
 }
 export default App
