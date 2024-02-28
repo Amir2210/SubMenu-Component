f41d35cfa7c25ec15e0416aed002bc6e30d3bf25
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-28 15:58:47 +0200

    Render the links of each subject

--- src/cmps/Sidebar.jsx
+++ src/cmps/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useGlobalContext } from '../AppContext'
-
+import sublinks from '../data'
 export function Sidebar() {
   const { isSidebarOpen } = useGlobalContext()
   return (
@@ -7,7 +7,14 @@ export function Sidebar() {
       <aside className='sidebar show-sidebar'>
         <div className="sidebar-container">
           <button className="close-btn">X</button>
-
+          <div className="sidebar-links">
+            {sublinks.map(link => <article key={link.pageId}>
+              <h4>{link.page}</h4>
+              <div className="sidebar-sublinks">
+                {link.links.map(icon => <a href={icon.url} key={icon.id}>{icon.icon}{icon.label}</a>)}
+              </div>
+            </article>)}
+          </div>
         </div>
       </aside>
     ) : null
