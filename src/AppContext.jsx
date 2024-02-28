bed501255877ef1bb484289effd23d08206b0b4f
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-28 13:44:41 +0200

    Render the specific icons when hover specific link in the navbar

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -27,7 +27,7 @@ export function AppContext({ children }) {
     setLinkHovered(null)
   }
   return (
-    <GlobalContext.Provider value={{ onOpenSidebar, onCloseSidebar, onLinkHover, onLinkRemoveHover }}>
+    <GlobalContext.Provider value={{ onOpenSidebar, onCloseSidebar, onLinkHover, onLinkRemoveHover, isLinkHover, linkHovered }}>
       {children}
     </GlobalContext.Provider>
   )
--- src/cmps/SubMenu.jsx
+++ src/cmps/SubMenu.jsx
@@ -1,7 +1,16 @@
+import { useGlobalContext } from '../AppContext'
+import sublinks from '../data'
 export function SubMenu() {
+  const { isLinkHover, linkHovered } = useGlobalContext()
+  const hoveredLink = sublinks.find(link => link.pageId === linkHovered)
   return (
-    <div className="submenu">
-      <h5>sdf</h5>
-    </div>
+    isLinkHover ? (
+      <div className="submenu show-submenu">
+        <h5>{hoveredLink.page}</h5>
+        <div className="submenu-links">
+          {hoveredLink.links.map(link => <a key={link.id}>{link.icon}{link.label}</a>)}
+        </div>
+      </div>
+    ) : null
   )
 }
